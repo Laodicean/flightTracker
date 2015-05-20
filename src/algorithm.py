f0f20296b035b7dd19597ae804b8dbe8e2a48dc8
Nathan Sher <nathys123452hotmail.com>
2015-05-20 22:05:25 +1000

    Altered the sort so that it now compiles

--- src/algorithm.py
+++ src/algorithm.py
@@ -4,39 +4,39 @@ def getFlightSolutions(query):
     return flightList
 
 def sortFlight(flightList,query):
-    swaps = 1
-    passes = 0
-    while (passes <query.numFlight-1 && swaps != 0):
-        counter = 0
-        swaps = 0
-        while (counter < query.numFlights - 1):
-            current = compare(flightList[counter],flightList[counter+1],query.pref1)
-           if (current == 1):
-               #swap
-               swaps = 1
-           elif (current == -1):
-               currPref2 = compare(flightList[counter],flightList[counter+1],query.pref2)
-               if (currPref2 == 1):
-                   #swap
-                   swap = 1
-               elif (currPref2 == -1):
-                   currPref3 = compare(flightList[counter],flightList[counter+1],query.pref3)
-                   if (currPref3 == 1):
-                       #swap
-                       swap = 1
-            counter ++
-        passes ++
+	swaps = 1
+	passes = 0
+	while (passes <query.numFlight-1 & swaps != 0):
+		counter = 0
+		swaps = 0
+		while (counter < query.numFlights - 1):
+			current = compare(flightList[counter],flightList[counter+1],query.pref1)
+			if current == 1:
+			#swap
+				swaps = 1
+			elif (current == -1):
+				currPref2 = compare(flightList[counter],flightList[counter+1],query.pref2)
+				if (currPref2 == 1):
+					#swap
+					swap = 1
+				elif (currPref2 == -1):
+					currPref3 = compare(flightList[counter],flightList[counter+1],query.pref3)
+					if (currPref3 == 1):
+						#swap
+						swap = 1
+			counter += 1
+		passes += 1
         
                     
 
 def compare(one,two,comparator):
-           if (comparator = POINT_PREF):
+           if (comparator == POINT_PREF):
                if (one.ffPoint > two.ffPoint):
                    return 1
                elif (one.ffPoint == two.ffPoint):
                    return -1
            else:
-               if(comparator = TIME_PREF):
+               if(comparator == TIME_PREF):
                    if(one.duration < two.duration):
                        return 1
                    elif(one.duration == two.duration):
