353f0de8599dcfa1d99825120b31b30700420c07
Nathan Sher <nathys123452hotmail.com>
2015-05-20 22:47:25 +1000

    Some changes to the algorithm document

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,7 +1,14 @@
+
 def getFlightSolutions(query):
     flightList[query.numFlights] = searchFlights(query)
     flightList = sortFlights(flightList,query)
+    print "THINK"
     return flightList
+
+def searchFlights(query):
+	import queue as Q
+	openQueue = Q.queue
+
 
 def sortFlight(flightList,query):
 	swaps = 1
@@ -12,17 +19,17 @@ def sortFlight(flightList,query):
 		while (counter < query.numFlights - 1):
 			current = compare(flightList[counter],flightList[counter+1],query.pref1)
 			if current == 1:
-			#swap
+				solutionSwap(flightList,counter);
 				swaps = 1
 			elif (current == -1):
 				currPref2 = compare(flightList[counter],flightList[counter+1],query.pref2)
 				if (currPref2 == 1):
-					#swap
+					solutionSwap(flightList,counter);
 					swap = 1
 				elif (currPref2 == -1):
 					currPref3 = compare(flightList[counter],flightList[counter+1],query.pref3)
 					if (currPref3 == 1):
-						#swap
+						solutionSwap(flightList,counter);
 						swap = 1
 			counter += 1
 		passes += 1
@@ -47,3 +54,9 @@ def compare(one,two,comparator):
                    elif (one.cost < two.cost):
                        return -1
            return 0
+
+
+def solutionSwap(flightList,counter):
+	temp = flightList[counter]
+	flightList[counter] = flightList[counter + 1]
+	flightList[coutner +1] = temp
