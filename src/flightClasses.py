59d910a534df41417df9539833cacfb9866b0766
gregory chernishov <gregory.chernishov@gmail.com>
2015-05-20 16:26:54 +1000

    -include Trip class -Housing for the algorithm -Sort for algorithm result

--- src/flightClasses.py
+++ src/flightClasses.py
@@ -9,6 +9,17 @@ Class Flight:
 		self.cost = cost
 
 
+Class Trip:
+        def __init__(self,date,time,start,end,duration,cost,ffPoint,listFlights):
+                self.date = date
+		self.time = time
+		self.start = start
+		self.end = end
+		self.duration = duration
+		self.cost = cost
+		self.ffPoint = ffPoint
+		self.listFlights = listFlights
+                
 Class Query:
 	def __init__(self,date,time,start,end,pref1,pref2,pref3,numFlights):
 		self.date = date
@@ -20,4 +31,9 @@ Class Query:
 		self.pref3 = pref3
 		self.numFlights = numFlights
 
+
+
+COST_PREF = 0
+TIME_PREF = 0
+POINT_PREF = 0
 # As a data structure for the results, since we can store flightplans as a list of flights, we'll be using an array of flightPlans (ordred by preference)
