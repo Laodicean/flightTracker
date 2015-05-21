2f1c95a9844be4534cf73b08ba8b4bba0e2c6d74
Nathan Sher <nathys123452hotmail.com>
2015-05-21 10:37:09 +1000

    Added functions for updating flight solutions

--- src/flightClasses.py
+++ src/flightClasses.py
@@ -10,15 +10,26 @@ Class Flight:
 
 
 Class Trip:
-        def __init__(self,date,time,start,end,duration,cost,ffPoint):
+        def __init__(self,date,time,start,end,duration,cost,ffPoint,airlinePref):
         	self.date = date
 			self.time = time
 			self.start = start
+			self.current = start
 			self.end = end
 			self.duration = duration
 			self.cost = cost
 			self.ffPoint = ffPoint
+			self.airlinePref = airlinePref
 			self.listFlights = []
+
+		def appendFlight(self,newFlight):
+			self.current = newFlight.end
+			self.duration += newFlight.duration
+			self.cost += newFlight.cost
+			if self.airlinePref == newFlight.airline:
+				self.ffPoint += newFlight.getFFPoints()
+			self.listFlights.append(newFlight)
+
                 
 Class Query:
 	def __init__(self,date,time,start,end,pref1,pref2,pref3,numFlights):
