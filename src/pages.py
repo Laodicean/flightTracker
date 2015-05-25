d84a4f37be346ad7e90c92381a017ad019a3eacd
Brendan Roy <br3ndanr>
2015-05-25 11:26:46 +0000

    Structure cleaned up and working properly

--- src/pages.py
+++ src/pages.py
@@ -10,17 +10,12 @@ class LandingHandler(tornado.web.RequestHandler):
     def get(self):
         loader = tornado.template.Loader("templates/")
         self.write(loader.load("landing.html").generate())
-    def post(self):
 
 
 class QueryHandler(tornado.web.RequestHandler):
     """Class which handles a request for flights"""
-    counter = 0;
     def get(self):
-        self.write("Hello get " + str(self.counter))
-        self.counter += 1
+        self.write("Error")
     def post(self):
-        self.write("Hello post " + str(self.counter))
-        self.counter -= 1
-
-       get(self)
+        loader = tornado.template.Loader("templates/")
+        self.write(loader.load("queryResponse.html").generate())
