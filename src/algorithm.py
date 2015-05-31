57a7003b44418f848197f61969df30544295c9cc
Brendan Roy <br3ndanr>
2015-05-31 16:23:01 +1000

    Additional progress on test.py

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
 import flightClasses
-#import Queue #no module named queue?
+from operator import itemgetter
+from queue import Queue #no module named queue?
 
 def getFlightSolutions(query,graph):
     """Retrieves a list of Trips containing soultions to the given query
     query: query object containing parameters for the search
     graph: Graph of cities
     """
-    flightList[query.numFlights] = searchFlights(query,graph)
+    flightList = searchFlights(query,graph)
     flightList = sortFlights(flightList,query)
     return flightList
 
 def searchFlights(query,graph):
-    q = Queue.Queue()
-    visited = Queue.Queue()
+    q = Queue()
+    visited = Queue()
     openQueue = q
     closedQueue = visited
     solutions = []
     solCounter = 0
-    firstTrip = flightClasses.Trip(query.date,query.time,query.start,query.end,0,0,0)
+    firstTrip = flightClasses.Trip(query.date,query.time,query.start,query.end,0,0,0,"")
     openQueue.put(firstTrip)
-    while( not openeQueue.empty() & solCounter < query.numFlights):
-        currTrip = openeQueue.get()
+    while( not openQueue.empty() & solCounter < query.numFlights):
+        currTrip = openQueue.get()
         if (currTrip.current == currTrip.end):
             solutions.append(currTrip)
             solCounter += 1
@@ -38,51 +39,22 @@ def searchFlights(query,graph):
     return solutions
 
 
-def sortFlight(flightList,query):
-    swaps = 1
-    passes = 0
-    while (passes <query.numFlight-1 & swaps != 0):
-        counter = 0
-        swaps = 0
-        while (counter < query.numFlights - 1):
-            current = compare(flightList[counter],flightList[counter+1],query.pref1)
-            if current == 1:
-                solutionSwap(flightList,counter);
-                swaps = 1
-            elif (current == -1):
-                currPref2 = compare(flightList[counter],flightList[counter+1],query.pref2)
-                if (currPref2 == 1):
-                    solutionSwap(flightList,counter);
-                    swap = 1
-                elif (currPref2 == -1):
-                    currPref3 = compare(flightList[counter],flightList[counter+1],query.pref3)
-                    if (currPref3 == 1):
-                        solutionSwap(flightList,counter);
-                        swap = 1
-            counter += 1
-        passes += 1
-
-def compare(one,two,comparator):
-    if (comparator == flightClasses.POINT_PREF):
-        if (one.ffPoint > two.ffPoint):
-            return 1
-        elif (one.ffPoint == two.ffPoint):
-            return -1
-    else:
-        if(comparator == flightClasses.TIME_PREF):
-            if(one.duration < two.duration):
-                return 1
-            elif(one.duration == two.duration):
-                return -1
-        else:
-            if (one.cost < two.cost):
-                return 1
-            elif (one.cost < two.cost):
-                return -1
-    return 0
+def sortFlights(flightList,query):
+    if query.pref1 == "cost":
+        if query.pref2 == "time":
+            return sorted(flightList, key=lambda f:(f.cost, f.curCal - f.startCal, f.ffPoint))
+        else:#ffPoint
+            return sorted(flightList, key=lambda f:(f.cost, f.ffPoint, f.curCal - f.startCal))
+    elif query.pref1 == "time":
+        if query.pref2 == "cost":
+            return sorted(flightList, key=lambda f:(f.curCal - f.startCal, f.cost, f.ffPoint))
+        else: #ffPoint
+            return sorted(flightList, key=lambda f:(f.curCal - f.startCal, f.ffPoint, f.cost))
+    else: #ffPoint
+        if query.pref2 == "cost":
+            return sorted(flightList, key=lambda f:(f.ffPoint, f.cost, f.curCal - f.startCal))
+        else: #time
+            return sorted(flightList, key=lambda f:(f.ffPoint, f.curCal - f.startCal, f.cost))
 
 
-def solutionSwap(flightList,counter):
-    temp = flightList[counter]
-    flightList[counter] = flightList[counter + 1]
-    flightList[coutner +1] = temp
+
